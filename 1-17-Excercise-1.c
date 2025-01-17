332695f82884cc6037337b419c33d2afabf37663
SuperSojh <supersojh@gmail.com>
2025-01-17 13:54:44 -0500

    c excercises from january 17th

---
+++ 1-17-Excercise-1.c
@@ -0,0 +1,11 @@
+void main() {
+
+    int num = 10;
+
+    do{
+        printf("%d\n", num);
+        num--;
+    } while (num >= 1);
+
+    printf("Happy New Year!\n");
+}
---
+++ 1-17-Excercise-2.c
@@ -0,0 +1,12 @@
+void main() {
+
+    int num = 10;
+
+    do{
+        if (num%2==0) 
+            printf("%d\n", num);
+        num--;
+    } while (num >= 0);
+
+
+}
